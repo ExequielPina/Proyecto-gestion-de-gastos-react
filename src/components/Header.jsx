c52c71b6a0869e08190ac0a2be29c20b74717363
ExequielPina <exequiel_pina@hotmail.es>
2022-11-29 11:39:38 +0100

    validación del formulario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { NuevoPresupuesto } from './NuevoPresupuesto'
+import NuevoPresupuesto from './NuevoPresupuesto'
 
-export const Header = () => {
+export const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
   return (
     <header>
         <h1>Planificador de gastos</h1>
 
-        <NuevoPresupuesto />
+        {isValidPresupuesto ? (
+          <p>Control de presupuesto</p>
+        ) : (
+          <NuevoPresupuesto
+          presupuesto= {presupuesto}
+          setPresupuesto= {setPresupuesto}
+          setIsValidPresupuesto= {setIsValidPresupuesto}
+          />
+        )}
+
+       
     </header>
     
   )
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import { useState} from 'react'
+import Mensaje from './Mensaje'
 
-export const NuevoPresupuesto = () => {
+  const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
+
+  //***STATE DE MENSAJES DE ERROR U OK***//
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    //***VALIDACIÓN DEL FORMULARIO***//
+    if (!presupuesto || presupuesto < 0) {
+          setMensaje('!No es un presupuesto válido¡')      
+          return
+    }
+    setMensaje('')
+    setIsValidPresupuesto(true)
+
+    
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>Definir presupuesto</label>
 
                 <input
                     className='nuevo-presupuesto'
-                    type="text"
+                    type="number"
                     placeholder='Añade tu presupuesto'
+                    value={presupuesto}
+                    onChange={ e => setPresupuesto (Number (e.target.value))}
                 />
             </div>
-            
-            <input type="submit" value="Añadir" />         
+
+            <input type="submit" value="Añadir" />    
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}     
         </form>
     </div>
   )
 }
+
+
+export default NuevoPresupuesto
