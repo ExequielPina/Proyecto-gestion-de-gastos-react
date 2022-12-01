2d3a2bdabe89e080fcb2f27b83c9bbd1bf8a2509
ExequielPina <exequiel_pina@hotmail.es>
2022-12-01 14:56:06 +0100

    agregando iconos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,11 +1,31 @@
 import { formaterFecha } from "../helpers";
 
+import Icono1 from '../img/icono_ahorro.svg'
+import Icono2 from '../img/icono_casa.svg'
+import Icono3 from '../img/icono_comida.svg'
+import Icono4 from '../img/icono_gastos.svg'
+import Icono5 from '../img/icono_ocio.svg'
+import Icono6 from '../img/icono_salud.svg'
+import Icono7 from '../img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+    ahorro: Icono1,
+    comida: Icono3,
+    hogar: Icono2,
+    ocio: Icono5,
+    salud: Icono6,
+    facturas: Icono7
+}
+
 const Gasto = ({gasto}) => {
 
   const { categoria, nombre, cantidad, id, fecha } = gasto;  
   return (
     <div className="gasto sombra">
         <div className="contenido-gasto">
+            <img
+                src= {diccionarioIconos[categoria]}
+            />
             <div className="descripcion-gasto">
                 <p className="categoria">{categoria}</p>
                 <p className="nombre-gasto">{nombre}</p>            
